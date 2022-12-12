1505ba6601913573bfd36eebe97a61fd79a9a9e1
dgfarm02 <dgfarm2@gmail.com>
2022-12-12 08:24:04 -0800

    assessment complete

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,11 @@
 // ASSESSMENT 2: Coding Practical Questions with Jest
 
+const { describe } = require("yargs")
+
+// const { it } = require("node:test")
+
+// const { it } = require("node:test")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,40 +20,151 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersArray1 = [6, 7, 8, 9, 10]
-// Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
+// const numbersArray1 = [6, 7, 8, 9, 10]
+// // Expected output: [18, 21, 24, 27, 30]
+// const numbersArray2 = [24, 27, 30, 33, 36]
 // Expected output: [72, 81, 90, 99, 108]
 
 // b) Create the function that makes the test pass.
 
 // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
 
+// PSEUDOCODE
+
+// create a function called objectDivided that takes in an object
+// parameters: one (object)
+// if number inside of object is divisible by 3 return "'number' is divisible by 3"
+// if number inside of object is not divisible by 3 return "'number' is not divisible by 3"
+// anything else return "error"
+
+
 // a) Create a test with expect statements for each of the variables provided.
 
-const object1 = { number: 15 }
-// Expected output: "15 is divisible by three"
-const object2 = { number: 0 }
-// Expected output: "0 is divisible by three"
-const object3 = { number: -7 }
-// Expected output: "-7 is not divisible by three"
+
+
+// describe("objectDivided", () => {
+//     it("takes in an object and checks if the number inside of it is divisible by 3", () => {
+//     expect(objectDivided(object1)).toEqual("15 is divisible by 3")
+//     expect(objectDivided(object2)).toEqual("0 is divisible by 3")
+//     expect(objectDivided(object3)).toEqual("-7 is not divisible by 3")
+//     const object1 = { number: 15 }
+//     // Expected output: "15 is divisible by three"
+//     const object2 = { number: 0 }
+//     // Expected output: "0 is divisible by three"
+//     const object3 = { number: -7 }
+//     // Expected output: "-7 is not divisible by three"
+//   })
+// })
+
+// Good failure!
+// ReferenceError: objectDivided is not defined
 
 // b) Create the function that makes the test pass.
+
+// const objectDivided = (object) => {
+//     if (object.number % 3 === 0) {
+//         return object.number + " is divisible by 3"
+//     } else if ( object.number % 3 !== 0) {
+//         return object.number + " is not divisible by 3"
+//     } else {
+//         return "error"
+//     }
+// }
+
+// console.log(objectDivided(object1))
+// console.log(objectDivided(object2))
+// console.log(objectDivided(object3))
+
+
+// PASS  ./code-challenges.test.js
+// ● Console
+
+//   console.log
+//     15 is divisible by 3
+
+//     at Object.log (code-challenges.test.js:68:9)
+
+//   console.log
+//     0 is divisible by 3
+
+//     at Object.log (code-challenges.test.js:69:9)
+
+//   console.log
+//     -7 is not divisible by 3
+
+//     at Object.log (code-challenges.test.js:70:9)
+
+
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
+// PSEUDOCODE
+// create a function called nounArray that takes in an array of strings
+// parameters: one (array)
+// iterate one index at a time and capitalize the first letter of each word in each index
+
+
+
 // a) Create a test with expect statements for each of the variables provided.
 
-const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
-// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
-// Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+// describe ("nounArray", () => {
+//     it("takes in an array and capitalizes the first letter the words in each index", () => {
+//         expect(nounArray(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+//         expect(nounArray(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango"])
+//         const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+//         // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+//         const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
+//         // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+//     })
+// })
+
+// Good Failure!
+// ReferenceError: nounArray is not defined
+
+
 
 // b) Create the function that makes the test pass.
 
+// const nounArray = (array) => {
+//     return array.map((value) => {
+//         return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase()
+//     })
+// }
+
+// console.log(nounArray(randomNouns1));
+// console.log(nounArray(randomNouns2));
+
+// PASS  ./code-challenges.test.js
+// ● Console
+
+//   console.log
+//     [ 'Streetlamp', 'Potato', 'Teeth', 'Conclusion', 'Nephew' ]
+
+//     at Object.log (code-challenges.test.js:129:9)
+
+//   console.log
+//     [ 'Temperature', 'Database', 'Chopsticks', 'Mango' ]
+
+//     at Object.log (code-challenges.test.js:130:9)
+
+
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
+// PSEUDOCODE
+// create a function called vowelMachine that takes in a string and 
+// parameters: one (string)
+// returns the number of the index where first vowel appears
+
 // a) Create a test with expect statements for each of the variables provided.
+
+
+describe("vowelMachine", () => {
+    it("takes in a string and logs the index of the first vowel", () => {
+    expect(vowelMachine(vowelTester1)).toEqual(1)
+    expect(vowelMachine(vowelTester2)).toEqual(0)
+    expect(vowelMachine(vowelTester3)).toEqual(2)
+    })
+})
 
 const vowelTester1 = "learn"
 // Expected output: 1
@@ -56,4 +173,20 @@ const vowelTester2 = "academy"
 const vowelTester3 = "challenges"
 // Expected output: 2
 
+
+// Good Failure: ReferenceError: vowelMachine is not defined
+
 // b) Create the function that makes the test pass.
+
+const vowelMachine = (str) => {
+    let strArr = str.split("")
+    for(i = 0; i < strArr.length; i++){
+        if(strArr[i] === "a" && strArr[i] === "e" && strArr[i] === "i" && strArr[i] === "o" || strArr[i] === "u"){
+            return str.indexOf(strArr[i])
+        }
+    }
+}
+
+
+
+
--- coding-concepts.js
+++ coding-concepts.js
@@ -8,36 +8,41 @@
 // --------------------1) What will this log?
 
 const cohort = "Hotel 2022"
-// console.log(cohort.split(""))
+console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: [H, o, t, e, l,  , 2, 0, 2, 2]
+// b) Verify and explain: .split takes the string Hotel 2022 and turns it into an array
+// [
+//   'H', 'o', 't', 'e',
+//   'l', ' ', '2', '0',
+//   '2', '2'
+// ]
 
 // --------------------2) What will this log?
 
 const greeter = (name) => {
   `Hello, ${name}!`
 }
-// console.log(greeter("LEARN Student"))
+console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This is a function, but it doesn't have a "return" i think this is going to be an error?
+// b) Verify and explain: undefined. but for the same reason as above, there is no return. 
 
 // --------------------3) What will this log?
 
 const multipliedByTwo = [4, 5, 6, 7, 8].map((number) => number * 2)
-// console.log(multipliedByTwo)
+console.log(multipliedByTwo)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:[8, 10, 12, 14, 16]
+// b) Verify and explain: ayy so the .map method is taking in the array and for every index in the array it will make an iteration that multiplies the value of the index by 2.
 
 // --------------------4) What will this log?
 
 const onlyOdds = [11, 12, 13, 14, 15].filter((number) => number % 2 !== 0)
-// console.log(onlyOdds)
+console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: [11, 13, 15]
+// b) Verify and explain: ayyy so .filter is going to create a new array with only the elements that fit the equation on the other side of the =>. The % is a modulo which just means not. this equation is saying 'the remainder of a number divided by 2 does NOT equal 0' or just another way of saying odd.
 
 // --------------------5) What will this log?
 
@@ -47,10 +52,10 @@ const myCodingSkills = {
   databases: "PostgreSQL",
   versionControl: "GitHub"
 }
-// console.log(myCodingSkills.languages[0])
+console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Javascript
+// b) Verify and explain: Correct! If you follow the pathway, the 0th (or first) index of the 'languages' (key) array is JavaScript. 
 
 // --------------------STRETCH: What will this log?
 
@@ -62,7 +67,7 @@ class Learn {
   }
 }
 const learnStudent = new Learn("George")
-// console.log(learnStudent)
+console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Hmm this is either going to create a new object using the instructions in the class called Learn, but i think something might be missing so it might not work? I'm not sure
+// b) Verify and explain: Learn { student: 'George', cohort: 'Hotel', year: 2022 }, ah so it did work. It created a new object using the template provided.
